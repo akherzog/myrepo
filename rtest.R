b721b931fcd5f8f9447a4e5fd557703f48dd09ac
akherzog <akabisch-herzog@mail.csuchico.edu>
2020-09-03 14:51:15 -0700

    test code

---
+++ rtest.R
@@ -0,0 +1,3 @@
+library(readxl)
+CHC_Projects_Post_Award_Master_List_Summaries_CURRENT <- read_excel("C:/Users/akabisch-herzog/Downloads/CHC Projects_Post Award_Master List & Summaries.CURRENT.xlsx")
+View(CHC_Projects_Post_Award_Master_List_Summaries_CURRENT)
